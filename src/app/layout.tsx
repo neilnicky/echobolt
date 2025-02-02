b3fa351ee0b7b4fe0c033a9f74f13a7b6d137b30
neilnicky <neilnicholaskanat@gmail.com>
2025-02-02 11:00:57 +0530

    feat: update font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Fira_Code } from 'next/font/google';
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Fira_Code({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'EchoBits - Real-time Website Analytics',

ff4d1c4c88cd5d7e7371bf81ec29862505c84c0b
neilnicky <neilnicholaskanat@gmail.com>
2025-02-02 12:35:45 +0530

    refactor: dashboard folder structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           {children}
           <Toaster />
         </ThemeProvider>
